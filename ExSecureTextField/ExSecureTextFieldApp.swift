06a2fbaff770ca84e6796c17b18c77fba8d57567
jake <palatable7@naver.com>
2022-07-26 01:16:58 +0900

    Initial Commit

---
+++ ExSecureTextField/ExSecureTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExSecureTextFieldApp.swift
+//  ExSecureTextField
+//
+//  Created by 김종권 on 2022/07/26.
+//
+
+import SwiftUI
+
+@main
+struct ExSecureTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
